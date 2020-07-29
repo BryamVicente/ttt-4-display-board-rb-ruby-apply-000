ba8dc084ad0919ccc4e05d2485725a3dd65970eb
Bryam Vicente <vicentebryam@gmail.com>
2020-07-29 01:52:42 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,8 +3,8 @@
 def display_board(spot)
 end
 
-puts "   |   |   "
-puts "-----------"
-puts "   |   |   "
-puts "-----------"
-puts "   |   |   "
+    |   |
+ -----------
+    |   |
+ -----------
+    |   |
